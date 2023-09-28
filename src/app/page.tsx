d7f3bdd60d0c41e66d36a1af7539970bca7ac628
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-09-28 01:48:47 -0300

    feat: :memo: added description to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Page() {
-  return null
+  return (
+    <>
+      <h1 className="flex items-center justify-center mt-48 text-3xl font-bold">
+        Prueba técnica de desarrollador Frontend para MELI
+      </h1>
+      <p className="flex items-center justify-center mt-4">
+        Para comenzar, ingresa un producto a buscar en la barra de búsqueda.
+      </p>
+    </>
+  )  
 }
